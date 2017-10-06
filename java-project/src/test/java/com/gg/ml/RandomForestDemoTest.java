27e3b609cdeb6c455e35f8b2b8513f6ad146d602
GowthamGirithar <g.srirangasamy@sap.com>
2017-10-06 09:30:09 +0530

    initial commit

---
+++ java-project/src/test/java/com/gg/ml/RandomForestDemoTest.java
@@ -0,0 +1,12 @@
+package com.gg.ml;
+import org.junit.Test;
+
+
+public class RandomForestDemoTest {
+	
+	@Test
+	public void test() throws Exception  {
+          RandomForestDemo.process();
+	}
+
+}
